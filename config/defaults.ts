197241204f442613e7a9a1ce66b8b9fb16d4437a
stefan.balea <stefan.balea@axsys.ro>
2023-01-28 02:14:38 +0200

    switch to testnet

--- config/defaults.ts
+++ config/defaults.ts
@@ -1,6 +1,6 @@
 import { networks } from "./networks";
 
-const CURRENT_NETWORK = 'mainnet'; //or 'mainnet' todo: move to .env
+const CURRENT_NETWORK = 'testnet'; //or 'mainnet' or 'testnet' todo: move to .env
 
 export const getChainName = (): string => {
     return networks[CURRENT_NETWORK].base.chainName;
